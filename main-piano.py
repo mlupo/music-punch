369b024760397a8958f5b567c81a102865b25b3c
Max Lupo <art@maxlupo.com>
2016-12-27 14:21:55 -0500

    This is a major change to the main-piano script to allow the use of the original Beep-Boop Machine Hardware and note cards in order to use more of the scale on the chord-organ.
    This changes the script from a real-time "light-sensor on = note on" system to
    one which reads some infromation, then executes that information when the
    command pin is activated.

--- main-piano.py
+++ main-piano.py
@@ -10,7 +10,6 @@ midiout1 = MidiOut(uart, ch=1)
 
 # set up the on board switch, and itial volume
 SW = pyb.Switch()
-volume = 5
 
 # each pin on the board is connected to a photo-resistor
 # which are mounted left-to-right in the device
@@ -22,7 +21,7 @@ STOP_PIN = pyb.Pin('A7', pyb.Pin.IN)
 FIVE = pyb.Pin('B13', pyb.Pin.IN)
 SIX = pyb.Pin('B14', pyb.Pin.IN)
 SEVEN = pyb.Pin('B15', pyb.Pin.IN)
-OCTAVE_UP = pyb.Pin('B3', pyb.Pin.IN)
+COMMAND = pyb.Pin('B3', pyb.Pin.IN)
 
 PIN_LIST = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN]
 
@@ -35,13 +34,6 @@ LED = pyb.Pin("B1", pyb.Pin.OUT_PP)
 # ADC pins, previously LIGHT = 1800 and DARK = 2200 were used
 ON = 0
 OFF = 1
-
-# set up pin PWM timer for output to buzzer or speaker
-# Pin B10 with timer 2 Channel 3
-BUZZER = pyb.Pin("B10")
-TIMER = pyb.Timer(2, freq=3000)
-CHANNEL = TIMER.channel(3, pyb.Timer.PWM_INVERTED,
-                        pin=BUZZER)
 
 C4 = 60
 D4 = 62
@@ -57,117 +49,121 @@ F5 = 77
 G5 = 79
 A5 = 81
 B5 = 83
+C6 = 84
 
-NOTE_LIST = [C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5]
+NOTE_LIST = [C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5, C6]
 
 
 class Boop:
     "class to monitor pins, and play the corresponding notes"
-    def __init__(self, base_note, scale_note, note_pin, notes, octave_pin):
+    def __init__(self, note_pin, note_range):
         "construct pins and notes"
-        self.base_note = base_note
-        self.scale_note = scale_note
-        self.notes = notes
         self.note_pin = note_pin
-        self.octave_pin = octave_pin
-        self.start_time = 0
-        self.on_timer = 0
-        self.oct_timer = 0
-        self.scale = 0
+        self.note_range = note_range
+        self.on_counter = 0
+        self.off_counter = 0
+        self.already_up = False
+        self.octave = -1
         self.note = 0
 
+    def initiliazer(self):
+        self.state_clear()
+        self.note = 0
+        self.octave = -1
+        self.already_up = False
 
-    def pin_check(self):
-        "check a given pin, and return its value"
+    def state_clear(self):
+        self.on_counter = 0
+        self.off_counter = 0
+
+    def note_check(self):
         if self.note_pin.value() == ON:
-            #self.octave_check()
-            self.on_timer += 1
-            if self.on_timer >= 5:
-                #self.on_timer = 0
-                return True
-            else:
-                return False
+            self.on_counter += 1
         elif self.note_pin.value() == OFF:
-            self.on_timer = 0
-            #self.octave_check()
-            #off_timer += 1
-            #if off_timer >= 5:
-            return False
+            self.off_counter += 1
 
-    def octave_check(self):
-        if self.octave_pin.value() == ON:
-            self.oct_timer += 1
-            #if self.oct_timer >= 2:
-            #print("scale is", self.scale)
-            self.scale = 7
-            self.note = self.notes[self.notes.index(self.base_note) + self.scale]
-        elif self.octave_pin.value() == OFF:
-            self.oct_timer = 0
-            #print("scale is", self.scale)
-            self.scale = 0
-            self.note = self.base_note
+
+
+class Commander:
+    "monitor the command pin"
+    def __init__(self, pin):
+        self.pin = pin
+        self.counter = 0
+        self.mode = False
+
+    def check(self):
+        if self.pin.value() == ON:
+             return True
+        elif self.pin.value() == OFF:
+            self.counter += 1
+            if self.counter >= 10:
+                self.counter = 0
+                return False
 
 
 # PROGRAM START ##########################
 
 LED.high()
 pyb.delay(100)
-playing_notes = []
+previous_notes = []
+current_notes = []
 play_times = []
+initial_check = 0
 
-C_NOTE = Boop(C4, C5, ONE, NOTE_LIST, OCTAVE_UP)
-D_NOTE = Boop(D4, D5, TWO, NOTE_LIST, OCTAVE_UP)
-E_NOTE = Boop(E4, E5, THREE, NOTE_LIST, OCTAVE_UP)
-F_NOTE = Boop(F4, F5, FOUR, NOTE_LIST, OCTAVE_UP)
-G_NOTE = Boop(G4, G5, FIVE, NOTE_LIST, OCTAVE_UP)
-A_NOTE = Boop(A4, A5, SIX, NOTE_LIST, OCTAVE_UP)
-B_NOTE = Boop(B4, B5, SEVEN, NOTE_LIST, OCTAVE_UP)
+C_NOTE = Boop(ONE, [C4, C5, C6])
+D_NOTE = Boop(TWO, [D4, D5])
+E_NOTE = Boop(THREE, [E4, E5])
+F_NOTE = Boop(FOUR, [F4, F5])
+G_NOTE = Boop(FIVE, [G4, G5])
+A_NOTE = Boop(SIX, [A4, A5])
+B_NOTE = Boop(SEVEN, [B4, B5])
 
 boop_list = [C_NOTE, D_NOTE, E_NOTE, F_NOTE, G_NOTE, A_NOTE, B_NOTE]
+command = Commander(COMMAND)
 
 while True:
     pyb.delay(45)
-    playing = STOP_PIN.value()
-    if playing == OFF:
+    stopping = STOP_PIN.value()
+    if stopping == OFF:
         servo.speed(-8)
         #pyb.delay(5)
-        for boopers in boop_list:
-            boopers.octave_check()
-            if boopers.pin_check():
-                #boopers.octave_check()
-#                pyb.delay(5)
-                if (boopers.note not in playing_notes):
-                    print("on", boopers.note)
-                    midiout1.note_on(boopers.note)
-                    #pyb.delay(5)
-                    boopers.start_time = pyb.millis()
-                    playing_notes.append(boopers.note)
-                    play_times.append(boopers.start_time)
-            if boopers.scale == 7 and boopers.base_note in playing_notes:
-                print("off", boopers.base_note)
-                midiout1.note_off(boopers.base_note)
-                #pyb.delay(5)
-                playing_notes.remove(boopers.base_note)
-            if boopers.scale == 0 and boopers.scale_note in playing_notes:
-                print("off", boopers.scale_note)
-                midiout1.note_off(boopers.scale_note)
-                #pyb.delay(5)
-                playing_notes.remove(boopers.scale_note)
-            if not boopers.pin_check():
-            # this would be true if the note for the selected pin is OFF
-                if boopers.base_note in playing_notes:
-                    print("off", boopers.base_note)
-                    midiout1.note_off(boopers.base_note)
-                    #pyb.delay(5)
-                    playing_notes.remove(boopers.base_note)
-                    #play_times.remove(boopers.start_time)
-                if boopers.scale_note in playing_notes:
-                    print("off", boopers.scale_note)
-                    midiout1.note_off(boopers.scale_note)
-                    #pyb.delay(5)
-                    playing_notes.remove(boopers.scale_note)
+        if not command.check():
+            if initial_check == 0:
+                for last_booped in boop_list:
+                    last_booped.state_clear()
+                    last_booped.initiliazer()
+                previous_notes = current_notes
+                current_notes = []
+                initial_check = 1
+            for boopers in boop_list:
+                boopers.note_check()
+                if boopers.on_counter >= 10:
+                    if not boopers.already_up:
+                        boopers.octave += 1
+                        if boopers.octave > len(boopers.note_range):
+                            boopers.octave = len(boopers.note_range - 1)
+                    boopers.state_clear()
+                if boopers.off_counter > 10 and boopers.octave > -1:
+                    boopers.already_up = False
+
+        elif command.check():
+            for booped in boop_list:
+                if booped.octave > -1:
+                    booped.note = booped.note_range[booped.octave]
+                    if (booped.note not in current_notes):
+                        print("on", boopers.note)
+                        midiout1.note_on(boopers.note)
+                        #pyb.delay(5)
+                        current_notes.append(booped.note)
+                for i in previous_notes:
+                    if i not in current_notes:
+                        print("off", i)
+                        midiout1.note_off(i)
+                        #pyb.delay(5)
+            initial_check = 0
+
     else:
-        if playing_notes:
+        if current_notes:
             print("midi all notes off signal")
             midiout1.all_sound_off()
             print("remove all notes")
@@ -177,10 +173,4 @@ while True:
             servo.speed(0, 800)
             pyb.delay(800)
 
-#    if SW() == True:
-#        volume += 1
-#        pyb.delay(200)
-#        if volume > 9:
-#            volume = 2
-#        print(volume)
 
